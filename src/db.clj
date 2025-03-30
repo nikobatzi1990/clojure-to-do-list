598ac8f041cbee644435d419185dec8127d54359
nikobatzi1990 <nicole.boci@gmail.com>
2025-03-30 01:14:17 -0400

    wip: connect database with conman

--- src/db.clj
+++ src/db.clj
@@ -1,20 +1,30 @@
 (ns db
   (:require [next.jdbc :as jdbc]
-            [migratus.core :as migratus]))
+            [migratus.core :as migratus]
+            [conman.core :as conman]))
 
 (def config {:store :database
              :migration-dir "resources/migrations"
              :db {:dbtype "h2"
-                  :dbname "to-do-list"
+                  :dbname "resources/db/to-do-list"
                   :host "localhost"
-                  :user "username"
-                  :password "password"}})
+                  :user "user"
+                  :password "pass"}})
 
 (def db {:dbtype "h2" :dbname "to-do-list"})
 (def ds (jdbc/get-datasource db))
 
+(migratus/init config)
+
 (defn migrate []
   (migratus/migrate config))
+
+(def datasource-options {:adapter "h2"
+                         :url     "jdbc:h2:~/to-do-list"
+                         :username      "user"
+                         :password      "pass"})
+
+(conman/connect! [datasource-options])
 
 (defn create-migration [name]
   (migratus.core/create config name))
@@ -23,5 +33,7 @@
   (migratus/rollback config))
 
 (migrate)
+
+(migratus/pending-list config)
 
 (jdbc/execute! ds ["select * from task"])
