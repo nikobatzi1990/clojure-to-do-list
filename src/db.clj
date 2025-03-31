569ad4f419178a80fe69922c3b7052c047429c47
nikobatzi1990 <nicole.boci@gmail.com>
2025-03-30 22:06:47 -0400

    feat: database connected

--- src/db.clj
+++ src/db.clj
@@ -1,43 +1,31 @@
 (ns db
-  (:require [next.jdbc :as jdbc]
-            [clojure.pprint :as pp]
-            [migratus.core :as migratus]
+  (:require [migratus.core :as migratus]
             [conman.core :as conman]))
 
 ;; conman config
-(def datasource-options {:adapter "h2"
-                         :url     "jdbc:h2:~/to-do-list"})
+(def db {:adapter "h2" 
+         :url "jdbc:h2:./resources/db/to-do-list"})
 
-(def ds
-  (let [conn (conman/connect! datasource-options)]
-    conn))
-
-(pp/pprint ds)
+(def ds (conman/connect! db))
 
 (conman/bind-connection "queries.sql")
 
+(defn disconnect! []
+  (conman/disconnect! ds))
+
+;; migratus config
 (def config {:store :database
              :migration-dir "resources/migrations"
              :db {:dbtype "h2"
-                  :dbname "resources/db/to-do-list"
-                  :host "localhost"
-                  :user "user"
-                  :password "pass"}})
+                  :dbname "./resources/db/to-do-list"}})
 
 (defn migrate []
   (migratus/migrate config))
 
 (migrate)
 
-(defn disconnect! [conn]
-  (conman/disconnect! conn))
-
 (defn create-migration [name]
   (migratus.core/create config name))
 
 (defn rollback-migration []
   (migratus/rollback config))
-
-(migratus/pending-list config)
-
-(jdbc/execute! ds ["select * from task"])
