1f3f6c75fc5138268cc810e2077f0f5272ca7ed9
nikobatzi1990 <nicole.boci@gmail.com>
2025-04-15 22:06:44 -0400

    feat: move app function to server.clj

--- src/clj/app/server.clj
+++ src/clj/app/server.clj
@@ -1,3 +1,8 @@
 (ns clj.app.server
   (:require
-   [shadow.cljs.devtools.server]))
+   [shadow.cljs.devtools.server]
+   [clojure.pprint :as pp]))
+
+(defn app [{:keys [query-string] :as req}]
+  (pp/pprint "Request: " req)
+  {:status 200 :body "Hello World!" :headers {"Content-Type" "text/html"}})
