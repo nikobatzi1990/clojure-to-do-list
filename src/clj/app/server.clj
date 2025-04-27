d17725fae0fccdaa905f76de69d8e46cf3a2f152
nikobatzi1990 <nicole.boci@gmail.com>
2025-04-26 22:43:44 -0400

    fix: remove print function that caused 500 error

--- src/clj/app/server.clj
+++ src/clj/app/server.clj
@@ -7,7 +7,6 @@
                [clojure.pprint :as pp]))
 
 (defn app [{:keys [query-string] :as req}]
-  (pp/pprint "Request: " req)
   {:status 200 :body "Hello World!" :headers {"Content-Type" "text/html"}})
 
 (def handler
