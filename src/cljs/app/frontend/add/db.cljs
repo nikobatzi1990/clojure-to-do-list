24e75312e5a4edd0f618ec2fdafc53bc5e8338fb
nikobatzi1990 <nicole.boci@gmail.com>
2025-06-07 22:56:17 -0400

    fix: add task db

--- src/cljs/app/frontend/add/db.cljs
+++ src/cljs/app/frontend/add/db.cljs
@@ -1,10 +1,8 @@
 (ns app.frontend.add.db
-  ;; (:require  [app.db :as db]
-  ;;            [next.jdbc :as jdbc]
-  ;;            [re-frame.core :as rf])
+  (:require [re-frame.core :as rf])
   )
 
-;; (rf/reg-event-db
-;;  :add/add-task
-;;  (fn [db [_ {:keys [result]}]]
-;;    (assoc db :add/result result)))
+(rf/reg-event-db
+ :add/add-task
+ (fn [db [_ {:keys [result]}]]
+   (assoc db :add/result result)))
