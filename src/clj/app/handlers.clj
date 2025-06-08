bec01403b67199da1bca6edfff7ed324b4504237
nikobatzi1990 <nicole.boci@gmail.com>
2025-06-07 22:44:01 -0400

    chore: add handler to save data to database

--- src/clj/app/handlers.clj
+++ src/clj/app/handlers.clj
@@ -1,1 +1,12 @@
-(ns app.handlers)
+(ns app.handlers
+  (:require [app.db :as db]
+            [next.jdbc :as jdbc]))
+
+(defn save-task [req]
+  (let [task (get-in req [:params :task])]
+    (jdbc/execute! {:datasource db/ds} [(str 
+                                         "insert into task(description)
+                                          values(" task ")")])
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+     :body {:message "Task saved!"}}))
