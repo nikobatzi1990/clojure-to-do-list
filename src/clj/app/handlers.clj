7ffbcd8abed69aa818c53d43066fc05276820498
nikobatzi1990 <nicole.boci@gmail.com>
2025-06-07 23:25:57 -0400

    fix: vulnerable sql query

--- src/clj/app/handlers.clj
+++ src/clj/app/handlers.clj
@@ -4,9 +4,8 @@
 
 (defn save-task [req]
   (let [task (get-in req [:params :task])]
-    (jdbc/execute! {:datasource db/ds} [(str 
-                                         "insert into task(description)
-                                          values(" task ")")])
+    (jdbc/execute! db/ds
+                   ["insert into task (description) values (?)" task])
     {:status 200
      :headers {"Content-Type" "application/json"}
      :body {:message "Task saved!"}}))
