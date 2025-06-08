1fa54eaa82a7de07e6a03f3cf60d848a0b8ba45f
nikobatzi1990 <nicole.boci@gmail.com>
2025-06-08 00:45:23 -0400

    feat: add handler for getting task list

--- src/clj/app/handlers.clj
+++ src/clj/app/handlers.clj
@@ -1,6 +1,18 @@
 (ns app.handlers
   (:require [app.db :as db]
-            [next.jdbc :as jdbc]))
+            [next.jdbc :as jdbc]
+            [hiccup2.core :as h]))
+
+(defn task-list [_ req]
+  (let [task-list 
+        (jdbc/execute! db/ds 
+                       ["select * from task"])]
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (str (h/html [:div
+                         [:ul
+                          (for [task task-list]
+                            [:li (:description task)])]]))}))
 
 (defn save-task [req]
   (let [task (get-in req [:params :task])]
