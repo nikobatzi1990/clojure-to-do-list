41c3e17ac3fbd2704e1d28c4d10fe8afc6d26e1f
nikobatzi1990 <nicole.boci@gmail.com>
2025-06-08 07:58:06 -0400

    wip: get components to display properly

--- src/cljs/app/frontend/main.cljs
+++ src/cljs/app/frontend/main.cljs
@@ -1,11 +1,39 @@
 (ns app.frontend.main
-  (:require [app.frontend.add.views :as add]
+  (:require [app.frontend.views :as main]
+            [app.frontend.add.views :as add]
             [app.frontend.add.events :as events]
             [app.frontend.add.db :as db]
+            [app.routes :as routes]
+            [reagent.dom.client :as rdc]
+            [re-frame.core :as rf]
             
-            ;; [reitit.frontend :as reititf]
-            [re-frame.core :as rf]))
+            [reitit.frontend :as reititf]
+            [reitit.frontend.easy :as rfe]))
+
+(rf/reg-event-db
+ :initialize-db              ;; usage: (dispatch [:initialise-db])
+ (fn [_ _]                   ;; Ignore both params (db and event)
+   {:add/task-input ""       ;; return a new value for app-db
+    }))
+
+(def routes
+  (reititf/router
+   routes/routes))
+
+(defonce app-root
+  (rdc/create-root (js/document.getElementById "app")))
+
+(defn ^:dev/after-load mount-ui []
+  (rdc/render app-root [main/main-ui]))
 
 (defn init []
-  (println "Hello World!"))
-(init)
+   (rf/dispatch-sync [:initialize-db])
+   (rf/clear-subscription-cache!))
+
+(rfe/start!
+  routes
+  (fn [new-match]
+    (rf/dispatch [:app/panel new-match]))
+  {:use-fragment false})
+
+ (mount-ui)
